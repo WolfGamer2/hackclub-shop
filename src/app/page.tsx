91a4b227e37bc7e1d3997fd73edb48af365b722e
Karthik Sankar <emergenitro@gmail.com>
2024-12-10 09:30:22 +0800

    Another fix for the merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Link from "next/link";
-import Navigation from "../../components/Navigation";
+import Navigation from "./components/Navigation";
 
 const MainPage = () => {
   return (
