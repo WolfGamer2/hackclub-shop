8750caa63e6afd9bfcd1e8d89627681c9470eaa4
Karthik Sankar <emergenitro@gmail.com>
2024-12-10 09:10:37 +0800

    Seriously, this should be the last change, hopefully

--- src/app/api/printful/products/route.ts
+++ src/app/api/printful/products/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
 export async function GET() {
 	const PRINTFUL_API_URL = process.env.PRINTFUL_API_URL!;
